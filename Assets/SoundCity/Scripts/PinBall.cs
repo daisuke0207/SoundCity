8259fcdf07fbc943c8b070222511a00e3da940ad
daisuke0207 <daisuke02074869@gmail.com>
2022-08-17 08:01:59 +0900

    Crate stage

---
+++ Assets/SoundCity/Scripts/PinBall.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SoundCity
+{
+    public class PinBall : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Start");
+        }
+    }
+}
